1d261204eab6c289973c0a6b02745a94aabd7f64
deliahbe <45146827+deliahbe@users.noreply.github.com>
2018-11-28 23:42:18 +0100

    update 1

--- index.php
+++ index.php
@@ -1,11 +1,69 @@
+<?php 
+    $text = $_REQUEST['Unfug1'] . " " . $_REQUEST['Unfug2'] . " " . $_REQUEST['Unfug3'] . "\n";
+    file_put_contents("formular.txt", $text, FILE_APPEND);
+?>
+
 <!DOCTYPE HTML>
 <html>
-<body>
 
-<?php 
+<head>
+    <title>Satzgenerator | Internet 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <style >
+        p {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+   <?php 
+        include('nav.inc');
+    ?>
+</body>
+    
+    
+<h1>SATZGENERATOR</h1>
+
+<p>
+    <?php 
     echo $_GET['Tageszeit'];
     echo $_GET['Name'];
+    ?>
+    !
+</p>
+
+<br>
+    
+<p>
+<b>Du sagst "JA!" zu...</b>
+<br>
+    <?php 
+    echo $_GET['Unfug1']; 
+    echo $_GET['Unfug2'];
+    echo $_GET['Unfug3'];
+    ?>
+</p>
+
+<br>
+
+<p>
+<b>Das wolltest Du noch loswerden:</b>
+<br>
+    <?php
+    echo $_GET['Textfeld'];
+    ?>
+</p>
+
+<?php
+    if (empty($_GET['Textfeld'])
+    die("Fehler! Kein Text übertragen.");
 ?>
     
-</body>
+<?php 
+    include('footer.inc');
+?>
+
+    
 </html>
